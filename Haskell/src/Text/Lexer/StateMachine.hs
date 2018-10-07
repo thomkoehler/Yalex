d984a4cf280ef1d308633d56e67ad7fe01c8328c
tkoehler <thom.koehler@gmail.com>
2018-10-07 12:08:56 +0200

    add StateMachine

--- Haskell/src/Text/Lexer/StateMachine.hs
+++ Haskell/src/Text/Lexer/StateMachine.hs
@@ -1,6 +1,8 @@
 
 module Text.Lexer.StateMachine where
 
+import Data.Foldable
+
 type State = Int
 type TransitionEntry = (State, Char -> Bool)
 type TransitionTable = [(State, (State, Char -> Bool))]
@@ -19,7 +21,7 @@ calcNextStates' :: StateMachine -> Char -> State -> [State]
 calcNextStates' stateMachine input state = 
   map fst . filter (\(_, pred) -> pred input) . map snd . filter (\(entryState, _) -> entryState == state) $ transitionTable stateMachine
 
-
+{-
 run :: StateMachine -> String -> Int
 run stateMachine text = foldl max 0 positions
   where
@@ -34,3 +36,23 @@ run stateMachine text = foldl max 0 positions
       where
         nextStates = calcNextStates stateMachine char currStates
         newAcceptedStates = zip (repeat pos) $ filter (`elem` acceptingStates') nextStates
+-}
+
+run :: StateMachine -> String -> Int
+run stateMachine text = foldl max 0 positions
+  where
+    acceptingStates' = acceptingStates stateMachine
+    positions = map fst $ snd $ go [initialState stateMachine] [] 1 text
+
+    go :: [State] -> [(Int, State)] -> Int -> String -> ([State], [(Int, State)])
+    go currStates acceptedStates pos currText = 
+      case currText of
+        [] ->  (currStates, acceptedStates)
+        (c:cs) -> 
+          let
+            nextStates = calcNextStates stateMachine c currStates
+            newAcceptedStates = zip (repeat pos) $ filter (`elem` acceptingStates') nextStates
+          in
+            if null nextStates
+              then (currStates, acceptedStates)
+              else go nextStates (if null newAcceptedStates then acceptedStates else newAcceptedStates) (pos + 1) cs
